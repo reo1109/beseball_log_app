945d38d9222d7c382fc47744c422b2f6760f4d82
rNom <reo1311090912@gmail.com>
2023-03-08 18:59:35 +0900

    新規登録画面作成

--- lib/view/create_account_page.dart
+++ lib/view/create_account_page.dart
@@ -1,4 +1,7 @@
+import 'package:baseball_log/utils/firebase/authentication.dart';
+import 'package:baseball_log/view/home_page.dart';
 import 'package:baseball_log/widget/widget_utils.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class CreateAccountPage extends StatefulWidget {
@@ -9,10 +12,56 @@ class CreateAccountPage extends StatefulWidget {
 }
 
 class _CreateAccountPageState extends State<CreateAccountPage> {
+  String email = '';
+  String pass = '';
+  String infoText = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: WidgetUtils.createAppBar('アカウント作成'),
+      body: Column(
+        children: [
+          const SizedBox(height: 32),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            child: TextFormField(
+              decoration: const InputDecoration(labelText: 'メールアドレス'),
+              onChanged: (String value) {
+                setState(() {
+                  email = value;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            child: TextFormField(
+              decoration: const InputDecoration(labelText: 'パスワード'),
+              obscureText: true,
+              onChanged: (String value) {
+                setState(() {
+                  pass = value;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 8),
+          ElevatedButton(
+              onPressed: () => {
+                Authentication.signUp(email: email, pass: pass),
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => HomePage()
+                  ),
+                ),
+              },
+              child: const Text('新規登録')
+          ),
+          Text(infoText),
+        ],
+      ),
     );
   }
 }
